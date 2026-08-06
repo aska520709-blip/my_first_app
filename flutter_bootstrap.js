7d5d704ce1e42aefecbedc02692b09fbd2351cfe
aska520709 <aska520709@gmail.com>
2026-08-06 22:31:41 +0900

    Fix GitHub Pages deployment and index entry

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2570490743" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1961701933" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
